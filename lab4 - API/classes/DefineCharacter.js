181daeadcb89269c624038a38ddc380e74f7d5bf
JakeDarb <stijn.bouckaert2209@icloud.com>
2021-03-17 01:53:20 +0100

    get character info

--- lab4 - API/classes/DefineCharacter.js
+++ lab4 - API/classes/DefineCharacter.js
@@ -11,10 +11,35 @@ class DefineCharacter{
             return response.json();
         })
         .then((json)=>{
-            console.log(json.age);
+            this.chooseCharacter(json.age);
         })
     }
-    
+
+    chooseCharacter(age){
+        let url = "https://rickandmortyapi.com/api/character/";
+        let characterId;
+        if(age<=5){
+            characterId = 472;
+        }else if(age<=16){
+            characterId = 353;
+        }else if(age<=25){
+            characterId = 231;
+        }else if(age<=40){
+            characterId = 72;
+        }else{
+            characterId = 381;
+        }
+        url = `https://rickandmortyapi.com/api/character/${characterId}`;
+        fetch(url)
+        .then((response) => {
+            return response.json();
+        })
+        .then((json)=>{
+            console.log(json.name);
+            console.log(json.image);
+        })
+    }
+
 }
 
-let character = new DefineCharacter("stijn");
+let character = new DefineCharacter("liam");
