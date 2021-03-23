ea93aab34ec8bca031ca83813a8806b6445ca668
JakeDarb <stijn.bouckaert2209@icloud.com>
2021-03-23 17:16:35 +0100

    setup: routers messages

--- lab5 - NodeJS/routes/api/v1/messages.js
+++ lab5 - NodeJS/routes/api/v1/messages.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
+router.get("/", (req, res)=>{
+    res.send("get basic");
+});
+
+router.get("/:id", (req, res)=>{
+    res.send("get using id");
+});
+
 router.post("/", (req, res)=>{
     res.send("success");
+});
+
+router.put("/:id", (req, res)=>{
+    res.send("update using id");
+});
+
+router.delete("/:id", (req, res)=>{
+    res.send("delete using id");
 });
 
 module.exports = router;
